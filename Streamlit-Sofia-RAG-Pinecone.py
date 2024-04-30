33b153fa65e9c3cf3e4c93fbabd44e790cf2facb
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-04-29 18:16:39 -0700

    Update Streamlit-Sofia-RAG-Pinecone.py

--- Streamlit-Sofia-RAG-Pinecone.py
+++ Streamlit-Sofia-RAG-Pinecone.py
@@ -55,6 +55,11 @@ ELEVEN_LABS_API_KEY= os.environ["ELEVEN_LABS_API_KEY"]
 client2= ElevenLabs(api_key= os.environ["ELEVEN_LABS_API_KEY"])
 PPLX_API_KEY= os.environ['PPLX_API_KEY']
 
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"]="ls__f58fcca57d5b430998efab563129b779"
+os.environ["LANGCHAIN_PROJECT"]="pt-uncommon-nexus-100"
+
 #Set up the Environment
 st.set_page_config(page_title="Sofia Vergara")
 assistant_logo = 'https://media.zenfs.com/en/shefinds_255/9feb1ca1273e3d3999cbd25b96c7f4c9'
@@ -97,21 +102,15 @@ We're at a casual happy hour, and I'm curious about your life. Please follow the
 -Keep your responses short, no longer than one paragraph with 100 characters. 
 -Ask follow-up questions or suggest related topics you think I'd find interesting.
 -You can talk about other topics broadly, but do not make up any details about Gloria or her beliefs if you can't find the related details within the document.
--Appropriately following the Guardrails provided:
 
-Guardrails:
-<grs>
-You should not speak about his wealth or net worth.
-You should not speak about Democrats, Republicans, or Donald Trump; or geopolitics in general.
-You should not speak with curse words.
-You should not speak about Suicide or Self-Harm.
-You should not speak about pornography or child pornography.
-You should not take a position on the Israel/Palestine conflict and should instead respond with a call for peace.
-</grs>
+=========
 Chat History:
 {chat_history}
-Question: {question}
 =========
+Question: 
+{question}
+=========
+Context:
 {context}
 =========
 """
@@ -131,21 +130,15 @@ We're at a casual happy hour, and I'm curious about your life. Please follow the
 -Ask follow-up questions or suggest related topics you think I'd find interesting.
 -You can talk about other topics broadly, but do not make up any details about Gloria or her beliefs if you can't find the related details within the document.
 -Respond in English unless requested otherwise.
--Appropriately following the Guardrails provided:
 
-Guardrails:
-<grs>
-You should not speak about her wealth or net worth.
-You should not speak about Democrats, Republicans, or Donald Trump; or geopolitics in general.
-You should not speak with curse words.
-You should not speak about Suicide or Self-Harm.
-You should not speak about pornography or child pornography.
-You should not take a position on the Israel/Palestine conflict and should instead respond with a call for peace.
-</grs>
+=========
 Chat History:
 {chat_history}
-Question: {question}
 =========
+Question: 
+{question}
+=========
+Context:
 {context}
 =========
 """
@@ -163,10 +156,16 @@ We're at a casual happy hour, and I'm curious about your life. Please follow the
 -Keep your responses short, no longer than one paragraph with 100 characters. 
 -Ask follow-up questions or suggest related topics you think I'd find interesting.
 -You can talk about other topics broadly, but do not make up any details about Gloria or her beliefs if you can't find the related details within the document.
--Appropriately following the Guardrails provided:
-{chat_history}
-Question: {question}
+
+
 =========
+Chat History:
+{chat_history}
+=========
+Question: 
+{question}
+=========
+Context:
 {context}
 =========
 """
@@ -174,6 +173,7 @@ Question: {question}
 # In case we want different Prompts for GPT and Llama
 Prompt_GPT = PromptTemplate(template=GPT_prompt_template, input_variables=["question", "context", "chat_history"])
 Prompt_Llama = PromptTemplate(template=Llama_prompt_template, input_variables=["question", "context", "chat_history"])
+Prompt_Claude = PromptTemplate(template=claude_prompt_template, input_variables=["question", "context", "system", "chat_history"])
 
 
 # Add in Chat Memory
@@ -198,9 +198,9 @@ def get_chatassistant_chain():
     embeddings = OpenAIEmbeddings()
     vectorstore = PineconeVectorStore(index_name="001-realavatar-sofia", embedding=embeddings)
     set_debug(True)
-    llm = ChatAnthropic(temperature=0, anthropic_api_key=api_key, model_name="claude-3-haiku-20240307", model_kwargs=dict(system=claude_prompt_template))
+    llm = ChatAnthropic(temperature=0, anthropic_api_key=api_key, model_name="claude-3-haiku-20240307", system="only respond in English")
     #llm = ChatAnthropic(temperature=0, anthropic_api_key=api_key, model_name="claude-3-opus-20240229", model_kwargs=dict(system=claude_prompt_template))
-    chain=ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
+    chain=ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory, combine_docs_chain_kwargs={"prompt": Prompt_Claude})
     return chain
 chain = get_chatassistant_chain()
 
